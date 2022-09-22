5d57b16aa3f6943f1668661070436b474500724d
Suave101 <57422635+Suave101@users.noreply.github.com>
2022-09-22 17:18:17 -0500

    Changed Stuff

--- Robot.py
+++ Robot.py
@@ -13,26 +13,24 @@ class MyRobot(wpilib.TimedRobot):
         self.addPeriodic(getNetworkTables, 5, offset=2)
         # Get Network tables Data || Define Things
         self.CANSparkMaxType = rev.CANSparkMaxLowLevel.MotorType(0)  # 0 is Brushed and 1 is Brushless
-        # TODO: Add all motors
-        self.SendableChooser = wpilib.SendableChooser()
-        self.SendableChooser.addOption()
-        # if self.prefs.containsKey("Front_Left_Motor"):
-        #     _temp = self.prefs.getString("Front_Left_Motor")
-        #     if _temp == "Spark_Max":
-        #         self._FLM = rev.CANSparkMax(0, self.CANSparkMaxType)
-        #     elif _temp == "Talon_FX":
-        #         # TODO: Add Talon_FX Functionality
-        #         pass
-        #     elif _temp == "Talon_SRX":
-        #         # TODO: Add Talon_SRX Functionality
-        #         pass
-        #     elif _temp == "Victor_SPX":
-        #         # TODO: Add Victor_SPX Functionality
-        #         pass
-        #     del _temp
-        # else:
-        #     self.prefs.initString("Front_Left_Motor", "Spark_Max")
-        #     self._FLM = rev.CANSparkMax(0, self.CANSparkMaxType)
+        self.prefs = wpilib.Preferences()
+        if self.prefs.containsKey("Front_Left_Motor"):
+            _temp = self.prefs.getString("Front_Left_Motor")
+            if _temp == "Spark_Max":
+                self._FLM = rev.CANSparkMax(0, self.CANSparkMaxType)
+            elif _temp == "Talon_FX":
+                # TODO: Add Talon_FX Functionality
+                pass
+            elif _temp == "Talon_SRX":
+                # TODO: Add Talon_SRX Functionality
+                pass
+            elif _temp == "Victor_SPX":
+                # TODO: Add Victor_SPX Functionality
+                pass
+            del _temp
+        else:
+            self.prefs.initString("Front_Left_Motor", "Spark_Max")
+            self._FLM = rev.CANSparkMax(0, self.CANSparkMaxType)
         # Define Game Stuff
         self.timer = wpilib.Timer()
 
