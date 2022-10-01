a6ba265950f1f4c453b9933bad1cb9c0b62dfc9a
Suave101 <57422635+Suave101@users.noreply.github.com>
2022-10-01 18:23:01 -0500

    Major Changes

--- Robot.py
+++ Robot.py
@@ -1,56 +1,38 @@
 import ctre
+import pyfrc.tests
 import rev
 import wpilib
 from networktables import NetworkTables
+from pyfrc.tests import *
 
 
 class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self):
-
         self.pdp = wpilib.PowerDistribution()
         self.bia = wpilib.BuiltInAccelerometer()
         NetworkTables.initialize()
         self.smartDash = NetworkTables.getTable("SmartDashboard")
 
-        self.brownStain = False  # Brownout Sustain
         self.brownoutDetection = True  # Enable Brownout Detection
 
         # Define Things
-        self.controllers = []
-        # TODO: Automate in the future: https://robotpy.readthedocs.io/projects/wpilib/en/stable/wpilib/CANStatus.html
-        # TODO: Automate by scanning data https://robotpy.readthedocs.io/projects/wpilib/en/stable/wpilib/CANData.html
-        # TODO: Make robot remember different controller configs https://robotpy.readthedocs.io/projects/wpilib/en/stable/wpilib/Preferences.html
-        # TODO: Do above with https://robotpy.readthedocs.io/projects/wpilib/en/stable/wpilib/DriverStation.html#wpilib.DriverStation.getJoystickType
-        # Order of controller ID's doesn't matter. Just the amount of each type
-        # Do controller config but with motors too
-
+        self.Motor_Controllers = {1: "RightSide", 9: "Shooter", 11: "Elevator", 2: "LeftSide", 5: "Intake"}
         self.CAN_Motors = []
-
-        # for x in range(1, 64):
-        #     try:
-        #         ccd = wpilib.CAN(deviceId=x)
-        #         if ccd.kTeamDeviceType == 2:
-        #             self.CAN_Motors.append({"ID": x, "Manufacturer": ccd.kTeamManufacturer})
-        #     except Exception as e:
-        #         pass
-
+        # Get all Motors
         for x in range(1, 64):
             try:
-                pass
-            except Exception as e:
-                pass
-            try:
-                pass
-            except Exception as e:
+                self.CAN_Motors.append({"Object": rev.CANSparkMax(deviceID=x, type=rev.CANSparkMaxLowLevel.MotorType(0)), "Type": "SparkMax", "ID": x})  # 0 is brushed 1 is brushless
+            except:
                 pass
             try:
+                self.CAN_Motors.append({"Object": ctre.TalonFX(x), "Type": "TalonFX", "ID": x})
+            except:
+                self.CAN_Motors.append({"Object": ctre.TalonSRX(x), "Type": "TalonSRX", "ID": x})
+            try:
+                self.CAN_Motors.append({"Object": ctre.VictorSPX(x), "Type": "VictorSPX", "ID": x})
+            except:
                 pass
-            except Exception as e:
-                pass
-
-
-
 
         self.prefs = wpilib.Preferences()
         self.CANSparkMaxType = rev.CANSparkMaxLowLevel.MotorType(0)  # 0 is Brushed and 1 is Brushless
@@ -60,7 +42,8 @@ class MyRobot(wpilib.TimedRobot):
             else:
                 self.prefs.initInt("Front_Left_Motor", "0")
             if self.prefs.getString("Front_Left_Motor") == "Spark_Max":
-                self._Front_Left_Motor = rev.CANSparkMax(int(self.prefs.getString("Front_Left_Motor_ID")), self.CANSparkMaxType)
+                self._Front_Left_Motor = rev.CANSparkMax(int(self.prefs.getString("Front_Left_Motor_ID")),
+                                                         self.CANSparkMaxType)
             elif self.prefs.getString("Front_Left_Motor") == "Talon_FX":
                 self._Front_Left_Motor = ctre.TalonFX(int(self.prefs.getString("Front_Left_Motor_ID")))
             elif self.prefs.getString("Front_Left_Motor") == "Talon_SRX":
@@ -69,7 +52,9 @@ class MyRobot(wpilib.TimedRobot):
                 self._Front_Left_Motor = ctre.VictorSPX(int(self.prefs.getString("Front_Left_Motor_ID")))
         else:
             self.prefs.initString("Front_Left_Motor", "Spark_Max")
-            self._Front_Left_Motor = rev.CANSparkMax(int(self.prefs.getString("Front_Left_Motor_ID")), self.CANSparkMaxType)
+            self._Front_Left_Motor = rev.CANSparkMax(int(self.prefs.getString("Front_Left_Motor_ID")),
+                                                     self.CANSparkMaxType)
+
         # Start Network Tables Stuff
 
         def getNetworkTables():
@@ -95,20 +80,24 @@ class MyRobot(wpilib.TimedRobot):
                                 self._Front_Left_Motor = ctre.VictorSPX(
                                     int(self.prefs.getString("Front_Left_Motor_ID")))
                             else:
-                                self.smartDash.putString(preference, f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
+                                self.smartDash.putString(preference,
+                                                         f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
                         if preference == "Front_Right_Motor":
                             if self.smartDash.getString(preference) == "Spark_Max":
                                 self._Front_Right_Motor = rev.CANSparkMax(
                                     int(self.prefs.getString("Front_Right_Motor_ID")), self.CANSparkMaxType)
                             elif self.smartDash.getString(preference) == "Talon_FX":
-                                self._Front_Right_Motor = ctre.TalonFX(int(self.prefs.getString("Front_Right_Motor_ID")))
+                                self._Front_Right_Motor = ctre.TalonFX(
+                                    int(self.prefs.getString("Front_Right_Motor_ID")))
                             elif self.smartDash.getString(preference) == "Talon_SRX":
-                                self._Front_Right_Motor = ctre.TalonSRX(int(self.prefs.getString("Front_Right_Motor_ID")))
+                                self._Front_Right_Motor = ctre.TalonSRX(
+                                    int(self.prefs.getString("Front_Right_Motor_ID")))
                             elif self.smartDash.getString(preference) == "Victor_SPX":
                                 self._Front_Right_Motor = ctre.VictorSPX(
                                     int(self.prefs.getString("Front_Right_Motor_ID")))
                             else:
-                                self.smartDash.putString(preference, f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
+                                self.smartDash.putString(preference,
+                                                         f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
                         if preference == "Back_Left_Motor":
                             if self.smartDash.getString(preference) == "Spark_Max":
                                 self._Back_Left_Motor = rev.CANSparkMax(
@@ -121,7 +110,8 @@ class MyRobot(wpilib.TimedRobot):
                                 self._Back_Left_Motor = ctre.VictorSPX(
                                     int(self.prefs.getString("Back_Left_Motor_ID")))
                             else:
-                                self.smartDash.putString(preference, f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
+                                self.smartDash.putString(preference,
+                                                         f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
                         if preference == "Back_Right_Motor":
                             if self.smartDash.getString(preference) == "Spark_Max":
                                 self._Back_Right_Motor = rev.CANSparkMax(
@@ -134,7 +124,8 @@ class MyRobot(wpilib.TimedRobot):
                                 self._Back_Right_Motor = ctre.VictorSPX(
                                     int(self.prefs.getString("Back_Right_Motor_ID")))
                             else:
-                                self.smartDash.putString(preference, f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
+                                self.smartDash.putString(preference,
+                                                         f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
                         if preference == "Shooter_Motor":
                             if self.smartDash.getString(preference) == "Spark_Max":
                                 self._Shooter_Motor = rev.CANSparkMax(
@@ -147,7 +138,8 @@ class MyRobot(wpilib.TimedRobot):
                                 self._Shooter_Motor = ctre.VictorSPX(
                                     int(self.prefs.getString("Shooter_Motor_ID")))
                             else:
-                                self.smartDash.putString(preference, f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
+                                self.smartDash.putString(preference,
+                                                         f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
                         if preference == "Intake_Motor":
                             if self.smartDash.getString(preference) == "Spark_Max":
                                 self._Intake_Motor = rev.CANSparkMax(
@@ -160,7 +152,8 @@ class MyRobot(wpilib.TimedRobot):
                                 self._Intake_Motor = ctre.VictorSPX(
                                     int(self.prefs.getString("Intake_Motor_ID")))
                             else:
-                                self.smartDash.putString(preference, f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
+                                self.smartDash.putString(preference,
+                                                         f"Invalid String - Defaulted to {self.prefs.getString(preference)}")
                         if preference == "Spark_Max_Brushed":
                             if self.prefs.containsKey("Spark_Max_Brushed"):
                                 self.CANSparkMaxType = rev.CANSparkMaxLowLevel.MotorType(
@@ -202,7 +195,7 @@ class MyRobot(wpilib.TimedRobot):
                 self.smartDash.setDefaultValue("PDP_Input_Voltage", 0)
 
             if self.smartDash.containsKey("Battery_Percentage"):
-                self.smartDash.putValue("Battery_Percentage", (self.pdp.getVoltage()-6.8)/5.2)
+                self.smartDash.putValue("Battery_Percentage", (self.pdp.getVoltage() - 6.8) / 5.2)
             else:
                 self.smartDash.setDefaultValue("Battery_Percentage", 0)
 
@@ -248,6 +241,20 @@ class MyRobot(wpilib.TimedRobot):
         # Define Game Stuff
         self.timer = wpilib.Timer()
 
+    def disabledInit(self):
+        for motor in self.CAN_Motors:
+            if motor["Type"] != "SparkMax":
+                motor["Object"].set(ctre.ControlMode.PercentOutput, 0)
+            else:
+                motor["Object"].set(0)
+
+    def disabledPeriodic(self):
+        for motor in self.CAN_Motors:
+            if motor["Type"] != "SparkMax":
+                motor["Object"].set(ctre.ControlMode.PercentOutput, 0)
+            else:
+                motor["Object"].set(0)
+
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
         self.timer.reset()
@@ -263,8 +270,12 @@ class MyRobot(wpilib.TimedRobot):
         #     self.drive.arcadeDrive(0, 0)  # Stop robot
 
     def teleopPeriodic(self):
-        # TODO: Implement pnumatics and motors
-        pass
+        for motor in self.CAN_Motors:
+            # ctre.ControlMode.PercentOutput
+            if motor["Type"] != "SparkMax":
+                motor["Object"].set(ctre.ControlMode.PercentOutput, 0)
+            else:
+                motor["Object"].set(0)
 
 
 if __name__ == "__main__":
