c7e5469f0e5a8c0d51146d98d8cecfafe4914ef7
Suave101 <57422635+Suave101@users.noreply.github.com>
2022-10-10 16:55:27 -0500

    Pneumatic stuffs

--- cecesrobot.py
+++ cecesrobot.py
@@ -10,6 +10,30 @@ class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self):
         self.pcm = wpilib.PneumaticsControlModule()
-        self.safePSI = 120
+        self.safePSI = 100
+        if self.pcm.getCompressor():
+            self.compressor = wpilib.Compressor(moduleType=self.pcm.getCompressorConfigType())
+            self.solenoid = self.pcm.makeSolenoid(1)  # Solenoid Number
+        else:
+            wpilib.reportWarning("Compressor Not Found!!!")
+
+    def disabledInit(self):
+        self.compressor.stop()
+        # if self.compressor.getPressure() > self.safePSI:
+        #     self.compressor.stop()
+        # else:
+        #     self.compressor.start()
+
+    def disabledPeriodic(self):
+        self.compressor.stop()
+        # if self.compressor.getPressure() > self.safePSI:
+        #     self.compressor.stop()
+        # else:
+        #     self.compressor.start()
 
     def teleopPeriodic(self):
+        if self.compressor.getPressure() > self.safePSI:
+            self.compressor.stop()
+        else:
+            self.compressor.start()
+
